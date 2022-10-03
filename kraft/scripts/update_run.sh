4c75598779c4a993464f6979dc0d702552df8f3b
Dave Shook <dshook@confluent.io>
2022-10-03 11:00:33 -0400

    updated to remove check for KAFKA_ADVERTISED_LISTENERS from /etc/confluent/docker/configure since this config setting is not specified for nodes that are only assigned the controller role

--- kraft/scripts/update_run.sh
+++ kraft/scripts/update_run.sh
@@ -3,6 +3,9 @@
 # Docker workaround: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
 sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
 
+# Docker workaround: Remove check for KAFKA_ADVERTISED_LISTENERS parameter
+sed -i '/dub ensure KAFKA_ADVERTISED_LISTENERS/d' /etc/confluent/docker/configure
+
 # Docker workaround: Ignore cub zk-ready
 sed -i 's/cub zk-ready/echo ignore zk-ready/' /etc/confluent/docker/ensure
 
